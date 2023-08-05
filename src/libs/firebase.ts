4236f90c6ee554084e79ee4de7b510dd95b194ec
kawabata324 <kawabataharuki48@gmail.com>
2023-08-05 21:49:53 +0900

    ✨ initialize firebase

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore/lite'
+import { getAuth } from 'firebase/auth'
 
 // Firebaseの設定を読み込む
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const auth = getAuth(app)
