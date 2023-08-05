e78e7eab77607a12779cf30da95f8599f13c53b2
kawabata324 <kawabataharuki48@gmail.com>
2023-08-05 21:53:44 +0900

    ✨ add admin link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
 import 'ress'
 
 export default function Home() {
-  return <main>Hello World</main>
+  return (
+    <main>
+      <Link href='/admins/register'>管理者登録</Link>
+    </main>
+  )
 }
