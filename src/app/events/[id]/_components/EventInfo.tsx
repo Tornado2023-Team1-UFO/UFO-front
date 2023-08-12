116384046b4cf67a2469bafd6a0df0f77aaee6d1
Asahi Kuroki <kurok011@umn.edu>
2023-08-12 16:36:36 +0900

    style eventInfo page

--- src/app/events/[id]/_components/EventInfo.tsx
+++ src/app/events/[id]/_components/EventInfo.tsx
@@ -1,4 +1,4 @@
-import styles from "@/app/events/[id]/eventdetails.module.css"
+import styles from "@/app/events/[id]/_components/eventinfo.module.css"
 import { use, useEffect, useState } from "react";
 import Image from "next/image";
 // --- firebase --- 
@@ -10,19 +10,26 @@ export default function EventInfo(data: any) {
     let imageURLs: string[] = [];
     const [loaded, setLoaded] = useState(false);
     const [userInfo, setUserInfo] = useState<DocumentData>();
+    const [startDate, setStartDate] = useState<Date>();
+    const [endDate, setEndDate] = useState<Date>();
     useEffect(() => {
-        if (Object.keys(data).length !== 0 && data.constructor === Object) {
-            setLoaded(true);
+        if (!loaded) {
+            if (Object.keys(data).length !== 0 && data.constructor === Object) {
+                setLoaded(true);
+            }
         }
     })
     useEffect(() => {
         if (loaded) {
             console.log(data);
             userId = data.userId._key?.path?.segments[6];
-            console.log("userID: " + userId);
             data.imageUrls.map((url: string) => {
                 imageURLs.push(url);
             });
+            // get date from data since date is UNIX timestamp
+            setStartDate(new Date(data.startAt)); 
+            setEndDate(new Date(data.endAt));
+            console.log("startDate: " + startDate);
             // contact firebase to get userInfo
             // currently userId is just a random number -> need get actual name and picture
             const fetchUserInfo = async () => {
@@ -52,37 +59,47 @@ export default function EventInfo(data: any) {
                     src={data.}
                 /> */}
             </div>
-            <div className={styles.eventinfo}>
-                <div>
+            <div className={styles.eventinfocontainer}>
+                <div className={styles.eventtitle}>
                     <h1>{data.title}</h1>
                 </div>
-                <div>
-                    {/* {userInfo ? ( */}
-                        <div>
-                            <Image
-                                src={userInfo?.photoURL}
-                                alt="user photo"
-                                width={50}
-                                height={50}
-                                /* style={{
-                                    objectPosition: 'center',
-                                }} */
-                            />
-                            <p>主催者名: {userInfo?.name}</p>
-                        </div>
-                    {/* ) : (
-                        <div>Loading user information...</div>
-                    )} */}
+                <div className={styles.eventhost}>
+                    <div className={styles.imagecontainer}>
+                        <Image
+                            src={userInfo?.photoURL}
+                            alt="user photo"
+                            fill
+                            // width={80}
+                            // height={80}
+                            style={{
+                                objectPosition: 'center',
+                            }}
+                        />
+                    </div>
+                    <div className={styles.eventhostname}>
+                        <p>主催者名: {userInfo?.name}</p>
+                    </div>
+                </div>
+                <div className={styles.eventdate}>
+                    <p>開催日時</p>
+                    {/* <p>`${startDate?.getMonth() + 1}</p> */}
+                </div>
+                <div className={styles.eventextrainfo}>
+                    <div className={styles.count}>
+                        <p>人数</p>
+                        <p>{data.recruitPeopleCounts}</p>
+                    </div>
+                    <div className={styles.location}>
+                        <p>Location</p>
+                    </div>
+                    <div className={styles.cost}> 
+                        <p>Cost</p>
+                    </div>
                 </div>
                 <div>
-                    <p>開催日: {data.date}</p>
+                    <p>イベント概要</p>
+                    <p>{data.content}</p>
                 </div>
-                <div>
-                    <p>人数: {data.recruitPeopleCounts}</p>
-                </div>
-            </div>
-            <div>
-                <p>イベント詳細: {data.outLine}</p>
             </div>
         </>
     );
