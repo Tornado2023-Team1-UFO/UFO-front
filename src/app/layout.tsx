c325e51d808ae1a4c3499b936f409888ce947ee4
Shinta <65914727+shinjatta@users.noreply.github.com>
2023-08-14 23:39:29 +0900

    Clerk SignIn SignUp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import 'ress'
+import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -10,8 +11,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './page.module.css'
-import { SignUp } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
     <div className={styles.center}>
-      <SignUp />
+      <SignIn />
     </div>
   )
 }
