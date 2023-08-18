78113e1b2231f8599d7e576031085f28bbfd5829
kawabata324 <kawabataharuki48@gmail.com>
2023-08-19 00:39:29 +0900

    ✨ TOP Pageの作成

--- src/components/button/StartButton.tsx
+++ src/components/button/StartButton.tsx
@@ -1,5 +1,13 @@
+import { FC } from 'react'
+import styles from './StartButton.module.css'
 type Props = {
   onClick: () => void
 }
 
-export const StartButton = () => {}
+export const StartButton: FC<Props> = ({ onClick }) => {
+  return (
+    <button className={styles.button} onClick={() => onClick()}>
+      <p className={styles.button_text}>はじめる</p>
+    </button>
+  )
+}
